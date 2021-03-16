417837bd8d334debdca5978c4cc2444828d97ed4
TheIcySpark <saacmanjarrez@gmail.com>
2021-03-16 08:58:04 -0600

    bases del menu

--- Menu/contenedor.gd
+++ Menu/contenedor.gd
@@ -1,9 +1,14 @@
 extends VBoxContainer
 
 onready var detallesDialog = get_parent().get_node("DetallesDialog")
+export var escenaJuego: String
+
 
 func _on_Jugar_button_down() -> void:
-	pass
+	get_tree().change_scene(escenaJuego)
 
 func _on_Detalles_button_down() -> void:
 	detallesDialog.popup()
+
+func _on_Quitar_button_down() -> void:
+	get_tree().quit()
